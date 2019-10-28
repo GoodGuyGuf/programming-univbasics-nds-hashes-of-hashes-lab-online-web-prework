511ad361fd58bebde1a1f33c30e33f17e2d12da8
Ethan <ethan_gustafson@outlook.com>
2019-10-28 10:20:58 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,29 +44,32 @@ def naming_system
   
  
  
-def naming_system = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class",
-      sub_category: {
-        label: "Order",
-        sub_category: {
-          label: "Family",
-          sub_category: {
+ label: "Kingdom",
+   sub_category: {
+     
+     label: "Phylum",
+     sub_category: {
+       
+       label: "Class",
+       sub_category: {
+         
+         label: "Order",
+         sub_category: {
+           
+           label: "Family",
+            sub_category: {
+              
             label: "Genus",
             sub_category: {
+              
               label: "Species",
               sub_category: nil
-}
-  }
-}
             }
-        }
-    }
+          }
+         }
+       }
+     }
+   }
   }
 
-end
-
-end
+multi_hash
